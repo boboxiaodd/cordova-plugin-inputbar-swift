0f9206b0739f07870e11614ae3be29da91afd7f2
bobo <iam386@gmail.com>
2019-10-30 20:13:12 +0800

    first commit

--- www/CDVInputBar.js
+++ www/CDVInputBar.js
@@ -3,25 +3,26 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = "CDVInputBar"; // This is just for code completion uses.
 
 var CDVInputBar = function() {}; // This just makes it easier for us to export all of the functions at once.
-/*
-options:
- type: photo , video , avatar
- size: Integer , max photo width or height
- filter: Bool , enable/disable show filter
 
-return:
-  photo / avatar:
-    url:   //tmpDirectory file with jpg file type
-    width: //photo's width
-    height: //photo's height
-    size: //file size
-
-  video:
-    url: //tmpDirectory file with mp4 file type
-    duration: //video duration
-    size: //file size
-*/
-CDVInputBar.open = function(onSuccess, onError , options) {
-    exec(onSuccess, onError, PLUGIN_NAME, "open", [options]);
+CDVInputBar.create = function(onSuccess, onError , options , buttons) {
+    exec(onSuccess, onError, PLUGIN_NAME, "create", [options,buttons]);
 };
+CDVInputBar.close = function(onSuccess,onError){
+    exec(onSuccess,onError, PLUGIN_NAME ,"close",[]);
+}
+CDVInputBar.hide = function(onSuccess,onError){
+    exec(onSuccess,onError,PLUGIN_NAME,"hide",[]);
+}
+CDVInputBar.show = function(onSuccess,onError){
+    exec(onSuccess,onError,PLUGIN_NAME,"show",[]);
+}
+CDVInputBar.play_sound = function(onSuccess,onError,path){
+    exec(onSuccess,onError,PLUGIN_NAME,"play_sound",[path]);
+}
+CDVInputBar.stop_sound = function(onSuccess,onError){
+    exec(onSuccess,onError,PLUGIN_NAME,"stop_sound",[]);
+}
+CDVInputBar.reset = function(onSuccess,onError){
+    exec(onSuccess,onError,PLUGIN_NAME,"reset",[]);
+}
 module.exports = CDVInputBar;
